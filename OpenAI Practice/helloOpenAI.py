e83c9ad96d45601d7253ec642d922d54568cb32e
adamfrly <adamfrly@gmail.com>
2022-01-20 21:40:18 -0500

    added intorductory pytorch stuff

--- OpenAI Practice/helloOpenAI.py
+++ OpenAI Practice/helloOpenAI.py
@@ -1,29 +1,16 @@
 import gym
 
-from stable_baselines import DQN
-from stable_baselines.common.evaluation import evaluate_policy
+from stable_baselines3 import A2C
 
+env = gym.make('CartPole-v1')
 
-# Create environment
-env = gym.make('LunarLander-v2')
+model = A2C('MlpPolicy', env, verbose=1)
+model.learn(total_timesteps=100000)
 
-# Instantiate the agent
-model = DQN('MlpPolicy', env, learning_rate=1e-3, prioritized_replay=True, verbose=1)
-# Train the agent
-model.learn(total_timesteps=int(2e5))
-# Save the agent
-model.save("dqn_lunar")
-del model  # delete trained model to demonstrate loading
-
-# Load the trained agent
-model = DQN.load("dqn_lunar")
-
-# Evaluate the agent
-mean_reward, std_reward = evaluate_policy(model, model.get_env(), n_eval_episodes=10)
-
-# Enjoy trained agent
 obs = env.reset()
 for i in range(1000):
-    action, _states = model.predict(obs)
-    obs, rewards, dones, info = env.step(action)
+    action, _state = model.predict(obs, deterministic=True)
+    obs, reward, done, info = env.step(action)
     env.render()
+    if done:
+      obs = env.reset()
